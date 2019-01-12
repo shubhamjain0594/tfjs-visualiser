381ba714d2d36dfe15b5bc935f1bd39dd6d46a05
Shubham Jain <shubhamjain0594@gmail.com>
2019-01-12 02:52:02 +0100

    Updates

---
+++ precache-manifest.3079c8f87ab0a7b68108fce7317ad04c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f7d05ab200b29d7acb53",
+    "url": "/tfjs-visualiser/static/js/runtime~main.f7d05ab2.js"
+  },
+  {
+    "revision": "232469d3283608af02b6",
+    "url": "/tfjs-visualiser/static/js/main.232469d3.chunk.js"
+  },
+  {
+    "revision": "acd944f8d2b57eec667c",
+    "url": "/tfjs-visualiser/static/js/1.acd944f8.chunk.js"
+  },
+  {
+    "revision": "232469d3283608af02b6",
+    "url": "/tfjs-visualiser/static/css/main.c0785701.chunk.css"
+  },
+  {
+    "revision": "acd944f8d2b57eec667c",
+    "url": "/tfjs-visualiser/static/css/1.045a99bb.chunk.css"
+  },
+  {
+    "revision": "d9a472e8249f44b5c8b1cd9eedaf6cdc",
+    "url": "/tfjs-visualiser/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3a53d8206f51219e6e99b2ce62d7ef5d.js"
+  "/tfjs-visualiser/precache-manifest.3079c8f87ab0a7b68108fce7317ad04c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/tfjs-visualiser/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
